4b938cd0a26a00138934005d5c0f11569a1d5a55
Lajos Cseppentő <1053931+LajosCseppento@users.noreply.github.com>
2022-11-30 08:40:41 +0100

    Minor

--- src/main/java/ch/cern/ycc/keycloakprovider/YccKeycloakProviderException.java
+++ src/main/java/ch/cern/ycc/keycloakprovider/YccKeycloakProviderException.java
@@ -6,10 +6,22 @@ package ch.cern.ycc.keycloakprovider;
  * @author Lajos Cseppento
  */
 public class YccKeycloakProviderException extends RuntimeException {
+
+  /**
+   * Default constructor.
+   *
+   * @param message message
+   */
   public YccKeycloakProviderException(String message) {
     super(message);
   }
 
+  /**
+   * Default constructor.
+   *
+   * @param message message
+   * @param cause cause
+   */
   public YccKeycloakProviderException(String message, Throwable cause) {
     super(message, cause);
   }
