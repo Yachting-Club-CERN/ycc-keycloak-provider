15c32645ffb423d7c749055d5daed91021d1571a
Lajos Cseppentő <1053931+LajosCseppento@users.noreply.github.com>
2023-03-23 22:14:28 +0100

    wip

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
     id("dev.lajoscseppento.ruthless.java-library")
+    id("org.sonarqube") version "4.0.0.2929"
 }
 
-val keycloakVersion = "20.0.1"
+val keycloakVersion = "21.0.1"
 
 dependencies {
     compileOnly("org.keycloak:keycloak-core:$keycloakVersion")
